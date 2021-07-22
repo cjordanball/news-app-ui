5b6a19c09780163684c142ad00f3184d1d75efbc
Jordan Ball <cjordanball@ballwebdev.com>
2021-07-21 21:33:45 -0400

    Initial Commit

---
+++ newsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newsApp
+//
+//  Created by C. Jordan Ball III on 7/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newsApp/newsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsAppApp.swift
+//  newsApp
+//
+//  Created by C. Jordan Ball III on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct newsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
